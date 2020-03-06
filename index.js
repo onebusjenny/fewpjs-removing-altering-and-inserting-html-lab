e951e639f3c4b3e53bb329786c23635ee38a287d
Jenny <jchudesign0930@gmail.com>
2020-03-06 01:25:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1');
+let newContent = document.createTextNode('Jenny is the champion')
+newHeader.appendChild(newContent)
+newHeader.id = 'victory';
+
+
+
+
+
